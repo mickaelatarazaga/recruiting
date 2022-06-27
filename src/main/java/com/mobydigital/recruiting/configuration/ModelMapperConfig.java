fdbb405275e4fbfa777b2b6cb7e59e6043f865a3
Micka Tarazaga <tarazaga.mickaela@gmail.com>
2022-06-27 12:20:55 -0300

    feat(Mapper) : Add configuration ModelMapper

---
+++ src/main/java/com/mobydigital/recruiting/configuration/ModelMapperConfig.java
@@ -0,0 +1,22 @@
+package com.mobydigital.recruiting.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+
+}
