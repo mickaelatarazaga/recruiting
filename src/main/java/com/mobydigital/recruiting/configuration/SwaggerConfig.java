d627e52d46754d579d25855e23a73bb649e890ff
Micka Tarazaga <tarazaga.mickaela@gmail.com>
2022-06-27 12:19:38 -0300

    fix(Swagger) : Correcting dependencies and editing the configuration

--- src/main/java/com/mobydigital/recruiting/configuration/SwaggerConfig.java
+++ src/main/java/com/mobydigital/recruiting/configuration/SwaggerConfig.java
@@ -1,41 +1,26 @@
 package com.mobydigital.recruiting.configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket apiDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.mobydigital.recruiting.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(getApiInfo())
-                ;
-    }
-
-    private ApiInfo getApiInfo() {
-        return new ApiInfo(
-                "Recruiting API REST",
-                "",
-                "1.0",
-                "http://codmind.com/terms",
-                new Contact("Mickaela Tarazaga", "https://www.linkedin.com/in/mickaelatarazaga/", "tarazaga.mickaela@gmail.com"),
-                "",
-                "",
-                Collections.emptyList()
+    public OpenAPI customOpenAPI(@Value("${project.version}") String appVersion) {
+        OpenAPI openApi = new OpenAPI();
+        openApi.info(
+                new Info()
+                        .title("Recruiting")
+                        .version(appVersion)
+                        .description(" ")
         );
+
+        return openApi;
     }
 }
+
