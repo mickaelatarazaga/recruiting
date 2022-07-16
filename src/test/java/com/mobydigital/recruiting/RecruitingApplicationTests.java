2ba60732212f6db324715ea5a0e97467113aef03
Micka Tarazaga <tarazaga.mickaela@gmail.com>
2022-07-16 19:35:08 -0300

    Delete unused test

--- src/test/java/com/mobydigital/recruiting/RecruitingApplicationTests.java
+++ src/test/java/com/mobydigital/recruiting/RecruitingApplicationTests.java
@@ -1,13 +1,8 @@
 package com.mobydigital.recruiting;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RecruitingApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
