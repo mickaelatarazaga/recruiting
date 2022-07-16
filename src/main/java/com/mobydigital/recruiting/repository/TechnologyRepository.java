1eb4e846fe1f0c1cbf7a1ce659fa5fdffd6d7d76
Micka Tarazaga <tarazaga.mickaela@gmail.com>
2022-07-16 19:11:53 -0300

    Delete unused method

--- src/main/java/com/mobydigital/recruiting/repository/TechnologyRepository.java
+++ src/main/java/com/mobydigital/recruiting/repository/TechnologyRepository.java
@@ -4,9 +4,6 @@ import com.mobydigital.recruiting.model.entity.Technology;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface TechnologyRepository extends JpaRepository<Technology, Long> {
-    Optional<Technology> findByNameAndVersion(String name, String version);
 }
