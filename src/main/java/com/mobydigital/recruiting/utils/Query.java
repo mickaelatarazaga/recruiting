2e715b9e65f3e276dd2a361d4399fcc23a861817
Micka Tarazaga <tarazaga.mickaela@gmail.com>
2022-07-16 13:05:37 -0300

    Fix Query declaration as Interface so they can't be instantiated

--- src/main/java/com/mobydigital/recruiting/utils/Query.java
+++ src/main/java/com/mobydigital/recruiting/utils/Query.java
@@ -1,10 +1,6 @@
 package com.mobydigital.recruiting.utils;
 
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-public class Query {
-
+public interface Query {
     public static final String FIND_BY_CANDIDATE_ID_AND_TECHNOLOGY_ID = "SELECT * FROM EXPERIENCES e WHERE e.candidate_id = ?1 AND e.technology_id = ?2";
     public static final String FIND_ALL_BY_CANDIDATE_ID = "SELECT * FROM EXPERIENCES e WHERE e.candidate_id = ?1";
 }
