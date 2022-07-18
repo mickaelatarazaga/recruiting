c28a7f368ac5c89e7f131c8de97cc62d9f1a2041
Micka Tarazaga <tarazaga.mickaela@gmail.com>
2022-07-18 10:12:55 -0300

    Add private constructor to hide the implicit public one

--- src/main/java/com/mobydigital/recruiting/utils/Query.java
+++ src/main/java/com/mobydigital/recruiting/utils/Query.java
@@ -1,5 +1,9 @@
 package com.mobydigital.recruiting.utils;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Query {
     public static final String FIND_BY_CANDIDATE_ID_AND_TECHNOLOGY_ID = "SELECT * FROM EXPERIENCES e WHERE e.candidate_id = ?1 AND e.technology_id = ?2";
     public static final String FIND_ALL_BY_CANDIDATE_ID = "SELECT * FROM EXPERIENCES e WHERE e.candidate_id = ?1";
