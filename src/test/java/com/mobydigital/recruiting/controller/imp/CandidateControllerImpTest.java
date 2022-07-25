a601a1dcb4d815841f69d3dbff34317df56d4db9
Micka Tarazaga <tarazaga.mickaela@gmail.com>
2022-07-25 12:18:41 -0300

    Delete unused annotation

--- src/test/java/com/mobydigital/recruiting/controller/imp/CandidateControllerImpTest.java
+++ src/test/java/com/mobydigital/recruiting/controller/imp/CandidateControllerImpTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.mobydigital.recruiting.util.TestUtil.getCandidateDto;
@@ -31,7 +30,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class CandidateControllerImpTest {
     @Autowired
--- src/test/java/com/mobydigital/recruiting/controller/imp/ExperienceControllerImpTest.java
+++ src/test/java/com/mobydigital/recruiting/controller/imp/ExperienceControllerImpTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.mobydigital.recruiting.util.TestUtil.getCandidateId;
@@ -32,7 +31,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class ExperienceControllerImpTest {
     @Autowired
